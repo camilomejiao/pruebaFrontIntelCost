79159c2e281265e5bf67447922f6c28e26fb99d6
Omar Mejia <omejia@sinergiaonline.com>
2020-08-16 17:08:22 -0500

    Se configura el services para traer y mostrar las imagenes

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+//Importante para que liste
+import { CommonModule } from "@angular/common";
+
+//Service
+import { PhotoService } from '../../services/photo.service';
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  //creamos el arrego de las fotos
+  photos = [];
+
+  constructor(
+    public _photoService: PhotoService
+  ) { }
 
   ngOnInit(): void {
+    this.search('all');
+  }
+
+  search(work){
+    console.log(work);
+    this._photoService.conectarImagenes(work).subscribe(
+      (resp: any) => {
+        if (resp.hits != ''){
+          console.log(resp);
+          console.log(resp.hits);
+          this.photos = resp.hits;
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
   }
 
 }
--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
+//IP global
+import { GLOBAL } from './global';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+
+import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operator/map';
+import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  public url: String;
+
+  constructor(
+    private _http: HttpClient
+  ) {
+    this.url = GLOBAL.url;
+  }
+
+  conectarImagenes(work){
+    let url = this.url + '?key=13119377-fc7e10c6305a7de49da6ecb25' +'&category='+work;
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this._http.get(url).map(
+      (resp: any)  => {
+        return resp;
+    });
+
+  }
+
+
 }
